02ea5c113fd0165de9a3d877cb912d17a550a8da
Esteban-PG <esteban.alp10@icloud.com>
2025-11-21 14:41:01 -0600

    se anade comprobacion para no correr siempre el etl

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
+import os
+
 from etl.etl_cleaning import run_etl
 from analysis.analysis_kpis import run_analysis
 
 def main():
 
-    df_clean = run_etl()
+    clean_path = "Dataset/credit_risk_dataset_clean.csv"
+
+    if not os.path.exists(clean_path):
+        print("No existe dataset limpio. Ejecutando ETL...")
+        run_etl()
+    else:
+        print("Dataset limpio ya existe. Saltando ETL.")
 
     resultados = run_analysis()
 
